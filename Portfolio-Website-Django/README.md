3c562030193ef8247236d0d35a9c626e5ded75ed
sudeep jadhav <156415995+sudeepjadhav542@users.noreply.github.com>
2024-08-23 23:23:15 +0530

    Add files via upload

---
+++ Portfolio-Website-Django/README.md
@@ -0,0 +1,2 @@
+# Portfolio-Website-Django
+Single Page Portfolio Website using Python Django-Framework, HTML CSS JavaScript Bootstrap
---
+++ Portfolio-Website-Django/staticfiles/mode.js
@@ -0,0 +1,5 @@
+
+// function changeMode(){
+//     var body = document.getElementById('body');
+//     body.classList.toggle('changeModes');
+// }
